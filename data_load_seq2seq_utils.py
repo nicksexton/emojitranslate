a4effde5ef0203158b3d6f5ad4204eb6fc4faee6
Nick Sexton <nick.sexton@gmail.com>
2018-12-20 18:58:20 +0000

    generators for x, y and emoji

--- data_load_seq2seq_utils.py
+++ data_load_seq2seq_utils.py
@@ -1,4 +1,4 @@
-""" Functions that load downloaded tweet/emoji data into a data frame and process it 
+""" Functions that load downloaded tweet/emoji data into a data frame and process it
     into numpy tran/dev/test sets for a Seq2Seq model.
 
     x is (tweet_length, character_set_size) sized ndarray"""
@@ -10,9 +10,29 @@ import numpy as np
 import data_load_utils as prev_util
 
 
-def xy_generator(tweets, batch_size=64, sequence_length=161):
-    """ Generator function that returns an (X, Y) tuple, where X and Y are a numpy 
-    array of shape (batch_size, sequence_length, len(char_indices) 
+# including newline character in CHARACTERS
+CHARACTERS = """\n '",.\\/|?:;@'~#[]{}-=_+!"£$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"""
+CHARACTERS_NO_NEWLINE = """ '",.\\/|?:;@'~#[]{}-=_+!"£$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"""
+
+
+def filter_text(text, chars=CHARACTERS_NO_NEWLINE):
+    """ takes an pd.Series of text. 
+    Filters it, removing twitter handles from text data - all text preceded by @ and then 
+    all characters not contained in universal set. Also removes all newline characters"""
+
+    def filter_handles(txt): return ' '.join(
+        word for word in txt.split(' ') if not word.startswith('@'))
+
+    def filter_chars(txt): return ''.join([c for c in txt if c in chars])
+
+    def filter(txt): return filter_chars(filter_handles(txt))
+
+    return text.apply(filter)
+
+
+def xy_generator(tweets, batch_size=64, sequence_length=161, emoji_indices=None):
+    """ Generator function that returns an (X, Y) tuple, where X and Y are a numpy
+    array of shape (batch_size, sequence_length, len(char_indices)
     sequence_length is 160 (longest tweet) + newline """
 
     # NB remeber to append \n to all sequences and include it in char_indices
@@ -25,6 +45,9 @@ def xy_generator(tweets, batch_size=64, sequence_length=161):
 
     x_dims = (batch_size, sequence_length, len(char_idx_univ))
     x_arr = np.zeros(shape=x_dims)
+    y_arr = np.zeros(shape=(batch_size, sequence_length-1, len(char_idx_univ)))
+    if emoji_indices:
+        emoji_arr = np.zeros(shape=(batch_size, len(emoji_indices)))
 
     while batch_num < n_batches:  # in case tweets < batch_size
 
@@ -34,9 +57,13 @@ def xy_generator(tweets, batch_size=64, sequence_length=161):
         for m in range(batch_size):
             for i, char in enumerate(this_batch.iloc[m].loc['text']):
                 x_arr[m, i, char_idx_univ[char]] = 1
+                if i > 0:
+                    # y_arr is ahead by one character and omits starting character
+                    y_arr[m, i-1, char_idx_univ[char]] = 1
+            if emoji_indices:
+                emoji_arr[m, emoji_indices[this_batch.iloc[m].loc['emoji']]] = 1
 
-        yield x_arr
-
-
-def emoji_generator(emoji_set=None):
-    return 'temp'
+        if emoji_indices:
+            yield (x_arr, y_arr, emoji_arr)
+        else:
+            yield (x_arr, y_arr)
--- test_data_load_seq2seq_utils.py
+++ test_data_load_seq2seq_utils.py
@@ -2,20 +2,86 @@
 
 
 import pandas as pd
-import data_load_seq2seq_utils as util
+import numpy as np
+import data_load_seq2seq_utils as s2s_util
+import data_load_utils as util
 
 
-def test_read_tweet_data_filters_headers():
-    """ filter out column headers (rows where text='text' and emoji='emoji') """
-    tweets = util.read_tweet_data('data/emojis_homemade.csv')
-    assert tweets.shape[0] > 0  # check tweets dataframe created
+def test_filter_text_filters_for_newlines():
+    """ check filters both handles and chars """
+    sample_series = pd.Series(
+        ['some text with a \nnewline',
+         'more \ntext with newline',
+         'I`m an undesirable character'],
+        name='text')
 
-    # run the filter again, check there's no hits
-    filt = tweets['emoji'] == 'emoji'
-    assert filt.any(axis=0) == False
+    compare_series = pd.Series(
+        ['some text with a newline',
+         'more text with newline',
+         'Im an undesirable character'],
+        name='text')
+
+    filtered_series = util.filter_text_for_handles(sample_series)
+    assert filtered_series[0] == compare_series[0]  # newline
+    assert filtered_series[1] == compare_series[1]  # newline
+    assert filtered_series[2] == compare_series[2]  # characters
+    return
 
 
-def test_import_and_filter_go():
-    """ import and filter text as it's intended to be used"""
+def test_xy_generator():
 
-    tweets = util.read_tweet_data('data/emojis_homemade.csv')
+    my_dict = {'text':
+               ["red and yellow and pink and green, orange and purple and blue, I can sing a rainbow, sing a rainbow, sing a rainbow too",
+                "sweet dreams are made of this, who am I to disagree, travel the world and the even seas, every body's looking for someone"],
+               'emoji':
+               [":rainbow:",
+                ":gay_pride_flag:"]}
+    my_data = pd.DataFrame(my_dict)
+
+    # copy the data 32 times so we have 64 rows
+    my_data_repeat = pd.concat([my_data]*32, ignore_index=True)
+
+    gen = s2s_util.xy_generator(my_data_repeat)
+
+    (x, y) = gen.__next__()
+
+    assert (x.shape[0] == 64)  # check generator returns an array with the right number of rows
+
+    # check x and y are identical but shifted once on axis 1
+    for i in range(x.shape[0]):
+        for j in range(1, x.shape[1]):
+            for k in range(x.shape[2]):
+                assert x[i, j, k] == y[i, j-1, k]
+
+
+def test_emoji_gen():
+    """ tests that xy_generator also makes an emoji generator """
+
+    my_dict = {'text':
+               ["red and yellow and pink and green, orange and purple and blue, I can sing a rainbow, sing a rainbow, sing a rainbow too",
+                "sweet dreams are made of this, who am I to disagree, travel the world and the even seas, every body's looking for someone"],
+               'emoji':
+               [":rainbow:",
+                ":gay_pride_flag:"]}
+    my_data = pd.DataFrame(my_dict)
+
+    # copy the data 32 times so we have 64 rows
+    my_data_repeat = pd.concat([my_data]*32, ignore_index=True)
+
+    gen = s2s_util.xy_generator(my_data_repeat)
+
+    emojis = sorted(list(set(my_data_repeat['emoji'])))
+    emoji_idx = dict((emoji, emojis.index(emoji)) for emoji in emojis)
+
+    gen = s2s_util.xy_generator(my_data_repeat, emoji_indices=emoji_idx)
+
+    (x, y, emoj) = gen.__next__()
+    # check emoji array has the right batch size:
+    assert emoj.shape[0] == 64
+
+    # check emoji array is the right shape
+    assert emoj.shape[1] == len(emoji_idx)
+
+    # check each row of emoji array sums to exactly one
+    for m in range(emoj.shape[0]):
+        assert np.sum(emoj[m, :]) == 1
